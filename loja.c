cfb6d46426cae27a8f378cf049b89ef1feeee398
leooqzy <lemes7848@gmail.com>
2026-04-05 19:43:28 -0300

    feat: implementação da inserção de contas na lista

--- loja.c
+++ loja.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Estrutura da conta
 typedef struct Conta {
     int id;
     char nick[50];
@@ -10,17 +10,53 @@ typedef struct Conta {
     struct Conta *prox;
 } Conta;
 
-// Inicializar lista
 void inicializar(Conta **lista) {
     *lista = NULL;
 }
 
+// Inserir conta
+void inserir(Conta **lista) {
+    Conta *novo = (Conta*) malloc(sizeof(Conta));
+
+    printf("ID: ");
+    scanf("%d", &novo->id);
+
+    printf("Nick: ");
+    scanf("%s", novo->nick);
+
+    printf("Nivel: ");
+    scanf("%d", &novo->nivel);
+
+    printf("Preco: ");
+    scanf("%f", &novo->preco);
+
+    novo->prox = NULL;
+
+    if (*lista == NULL) {
+        *lista = novo;
+    } else {
+        Conta *aux = *lista;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+
+    printf("Conta cadastrada!\n");
+}
+
 int main() {
     Conta *lista;
+    int opcao;
 
     inicializar(&lista);
 
-    printf("Lista inicializada com sucesso!\n");
+    printf("1 - Inserir conta\n");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) {
+        inserir(&lista);
+    }
 
     return 0;
 }
